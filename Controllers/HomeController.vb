e3b2d3329a5da91d172eb8ff02546d9af0e139cd
mmglsai <70984846+mmglsai@users.noreply.github.com>
2020-11-16 22:38:10 -0500

    Update HomeController.vb

--- Controllers/HomeController.vb
+++ Controllers/HomeController.vb
@@ -12,7 +12,7 @@
     End Function
 
     Function Contact() As ActionResult
-        ViewData("Message") = "Your contact page."
+        ViewData("Message") = "Your contact page.Changes"
 
         Return View()
     End Function
